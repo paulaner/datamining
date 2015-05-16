73cac92a4822d39515d87b18b16dea386df15744
paulaner <zhougf01@gmail.com>
2015-05-16 09:04:54 -0400

    add one basic comment on DBSCAN

--- datamining/DBSCAN.java
+++ datamining/DBSCAN.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// implementation of DBSCAN algorithm 
 public class DBSCAN {
 
     public static void main(String[] args) {
